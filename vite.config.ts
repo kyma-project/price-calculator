51151d9d0648148592af0cd00e5a1889505393ca
Damian Badura <damian.badura@sap.com>
2025-05-13 16:01:31 +0200

    another adjustment

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,12 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'build',
+  },
   plugins: [react()],
   server: {
     port: 3000,
   },
+
 });

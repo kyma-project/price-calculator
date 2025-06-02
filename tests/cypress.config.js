ba4e814be10cd60ae5202acec458ffdbc5430222
Damian Badura <damian.badura@sap.com>
2025-06-02 22:37:56 +0200

    add video

--- tests/cypress.config.js
+++ tests/cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   viewportWidth: 1500,
   viewportHeight: 1500,
+  video: true,
   e2e: {
     baseUrl: 'http://localhost:3000',
     supportFile: 'command.js',
